d617d95ef2a880c1ed62f51569035f5b065bad48
0verflowme <priyanshukumarpu@gmail.com>
2024-04-25 11:19:41 +0530

    refactorings
    Signed-off-by: 0verflowme <priyanshukumarpu@gmail.com>

--- client.go
+++ client.go
@@ -18,28 +18,24 @@ type Client struct {
 	Storage *storage.Client
 }
 
-type RestOptions struct {
+type Options struct {
+	Headers map[string]string
+	Db      *OptionsDb
+}
+
+type OptionsDb struct {
 	Schema string
 }
 
-type ClientOptions struct {
-	Headers map[string]string
-	Db      *RestOptions
-}
-
 // NewClient creates a new Supabase client.
-// url is the Supabase URL.
-// key is the Supabase API key.
-// options is the Supabase client options.
-func NewClient(url, key string, options *ClientOptions) (*Client, error) {
+func NewClient(url, key string, options *Options) (*Client, error) {
 	if url == "" || key == "" {
 		return nil, errors.New("url and key are required")
 	}
 
-	headers := map[string]string{
-		"Authorization": "Bearer " + key,
-		"apikey":        key,
-	}
+	headers := make(map[string]string)
+	headers["Authorization"] = "Bearer " + key
+	headers["apikey"] = key
 
 	if options != nil && options.Headers != nil {
 		for k, v := range options.Headers {
@@ -47,12 +43,9 @@ func NewClient(url, key string, options *ClientOptions) (*Client, error) {
 		}
 	}
 
-	var schema string
+	schema := DefaultSchema
 	if options != nil && options.Db != nil {
 		schema = options.Db.Schema
-	}
-	if schema == "" {
-		schema = DefaultSchema
 	}
 
 	client := &Client{
@@ -64,13 +57,11 @@ func NewClient(url, key string, options *ClientOptions) (*Client, error) {
 }
 
 // Wrap postgrest From method
-// From returns a QueryBuilder for the specified table.
 func (c *Client) From(table string) *postgrest.QueryBuilder {
 	return c.Rest.From(table)
 }
 
 // Wrap postgrest Rpc method
-// Rpc returns a string for the specified function.
 func (c *Client) Rpc(name, count string, rpcBody interface{}) string {
 	return c.Rest.Rpc(name, count, rpcBody)
 }
